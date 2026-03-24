9acebea97ef4228a06549b50d658e4824619daa8
Ace_Radom <sichenradomlyu@gmail.com>
2026-03-24 23:10:30 +0100

    add basic utils

---
+++ NovaLingua.Lib/Utils/Time.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NovaLingua.Lib.Utils;
+
+public static class Time
+{
+    public static long GetUtcNowTs() => DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+}
---
+++ NovaLingua.Lib/Utils/Uuid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NovaLingua.Lib.Utils;
+
+public static class Uuid
+{
+    public static string Generate()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
