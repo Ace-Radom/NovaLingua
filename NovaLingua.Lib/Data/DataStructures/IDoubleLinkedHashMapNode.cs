7f1668fa8cf52493b88f5817889c1ad21109422e
Ace_Radom <sichenradomlyu@gmail.com>
2026-03-27 11:23:10 +0100

    basic double linked hashmap function

---
+++ NovaLingua.Lib/Data/DataStructures/IDoubleLinkedHashMapNode.cs
@@ -0,0 +1,8 @@
+﻿namespace NovaLingua.Lib.Data.DataStructures;
+
+public interface IDoubleLinkedHashMapNode<T> where T : notnull
+{
+    public T Prev { get; set; }
+    public T Next { get; set; }
+    public uint Order { get; set; }
+}
